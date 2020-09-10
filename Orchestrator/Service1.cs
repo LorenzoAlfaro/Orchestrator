ea90b217dc5d305c1f1bd3ddd2be21d796daf357
Lorenzo <lorenzo@whirlwindsoftware.com>
2020-09-10 15:36:25 -0600

    first

---
+++ Orchestrator/Service1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Orchestrator
+{
+    public partial class MailService : ServiceBase
+    {
+        private Timer timer1= new Timer();
+        int getCallType;
+
+        public MailService()
+        {
+            InitializeComponent();
+            Scheduler();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.AutoReset = true;
+            timer1.Enabled = true;
+            ServiceLog.WriteErrorLog("Daily Reporting service started");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.AutoReset = false;
+            timer1.Enabled = false;
+            ServiceLog.WriteErrorLog("Daily Reporting service stopped");
+        }
+
+        public void Scheduler()
+        {
+            //InitializeComponent();
+            int strTime = Properties.Settings.Default.callDuration;
+            getCallType = Properties.Settings.Default.callType;
+            if (getCallType == 1)
+            {
+                timer1 = new Timer();
+                double inter = (double)GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+            }
+            else
+            {
+                timer1 = new Timer();
+                timer1.Interval = strTime * 1000;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+            }
+        }
+
+        private double GetNextInterval()
+        {
+            string timeString = Properties.Settings.Default.StartTime;
+            DateTime t = DateTime.Parse(timeString);
+            TimeSpan ts = new TimeSpan();
+            int x;
+            ts = t - System.DateTime.Now;
+            if (ts.TotalMilliseconds < 0)
+            {
+                ts = t.AddDays(1) - System.DateTime.Now;//Here you can increase the timer interval based on your requirments.   
+            }
+            return ts.TotalMilliseconds;
+        }
+
+        private void SetTimer()
+        {
+            try
+            {
+                double inter = (double)GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ServiceTimer_Tick(object sender, ElapsedEventArgs e)
+        {
+            string Msg = "Hi ! This is DailyMailSchedulerService mail.";//whatever msg u want to send write here.  
+                                                                        // Here you can write the   
+            ServiceLog.SendEmail("lorenzoac88@gmail.com", "", "", "Daily Report of DailyMailSchedulerService on " + DateTime.Now.ToString("dd-MMM-yyyy"), Msg);
+
+            if (getCallType == 1)
+            {
+                timer1.Stop();
+                System.Threading.Thread.Sleep(1000000);
+                SetTimer();
+            }
+        }
+    }
+}
---
+++ Orchestrator/ServiceLog.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrator
+{
+    public static class ServiceLog
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+        /// <summary>  
+        /// this function write Message to log file.  
+        /// </summary>  
+        /// <param name="Message"></param>  
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        #region Send Email Code Function  
+        /// <summary>  
+        /// Send Email with cc bcc with given subject and message.  
+        /// </summary>  
+        /// <param name="ToEmail"></param>  
+        /// <param name="cc"></param>  
+        /// <param name="bcc"></param>  
+        /// <param name="Subj"></param>  
+        /// <param name="Message"></param>  
+        public static void SendEmail(String ToEmail, string cc, string bcc, String Subj, string Message)
+        {
+            //Reading sender Email credential from web.config file  
+
+            string HostAdd = Properties.Settings.Default.Host;
+            string FromEmailid = Properties.Settings.Default.FromMail;
+            string Pass = Properties.Settings.Default.Password;
+
+            //creating the object of MailMessage  
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(FromEmailid); //From Email Id  
+            mailMessage.Subject = Subj; //Subject of Email  
+            mailMessage.Body = Message; //body or message of Email  
+            mailMessage.IsBodyHtml = true;
+
+            string[] ToMuliId = ToEmail.Split(',');
+            foreach (string ToEMailId in ToMuliId)
+            {
+                mailMessage.To.Add(new MailAddress(ToEMailId)); //adding multiple TO Email Id  
+            }
+
+
+            //string[] CCId = cc.Split(',');
+
+            //foreach (string CCEmail in CCId)
+            //{
+            //    mailMessage.CC.Add(new MailAddress(CCEmail)); //Adding Multiple CC email Id  
+            //}
+
+            //string[] bccid = bcc.Split(',');
+
+            //foreach (string bccEmailId in bccid)
+            //{
+            //    mailMessage.Bcc.Add(new MailAddress(bccEmailId)); //Adding Multiple BCC email Id  
+            //}
+            SmtpClient smtp = new SmtpClient();  // creating object of smptpclient  
+            smtp.Host = HostAdd;              //host of emailaddress for example smtp.gmail.com etc  
+
+            //network and security related credentials  
+
+            smtp.EnableSsl = false;
+            NetworkCredential NetworkCred = new NetworkCredential();
+            NetworkCred.UserName = mailMessage.From.Address;
+            NetworkCred.Password = Pass;
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = NetworkCred;
+            smtp.Port = 25;
+            smtp.Send(mailMessage); //sending Email  
+        }
+
+        #endregion
+    }
+}
